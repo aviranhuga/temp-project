5f54fdb1c5abd0ddf4b88fcc17e4640f2f23c53e
Aviran Huga <Aviranhuga@gmail.com>
2018-02-23 13:03:37 +0200

    finish all todo

--- flickr-gallery-master/src/components/Gallery/Gallery.js
+++ flickr-gallery-master/src/components/Gallery/Gallery.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import Image from '../Image';
 import './Gallery.scss';
+import InfiniteScroll from 'react-infinite-scroller';
 
 class Gallery extends React.Component {
   static propTypes = {
@@ -11,8 +12,10 @@ class Gallery extends React.Component {
 
   constructor(props) {
     super(props);
+    this.loadMore = this.loadMore.bind(this);
     this.state = {
       images: [],
+      numOfImages: 100,
       galleryWidth: this.getGalleryWidth()
     };
   }
@@ -25,8 +28,13 @@ class Gallery extends React.Component {
     }
   }
 
-  getImages(tag) {
-    const getImagesUrl = `services/rest/?method=flickr.photos.search&api_key=522c1f9009ca3609bcbaf08545f067ad&tags=${tag}&tag_mode=any&per_page=100&format=json&nojsoncallback=1`;
+  loadMore(pageToLoad){
+    const newnumOfImages = this.state.numOfImages + pageToLoad*30;
+    this.getImages(this.props.tag,newnumOfImages);
+  }
+
+  getImages(tag, numOfImages) {
+    const getImagesUrl = `services/rest/?method=flickr.photos.search&api_key=522c1f9009ca3609bcbaf08545f067ad&tags=${tag}&tag_mode=any&per_page=${numOfImages}&format=json&nojsoncallback=1`;
     const baseUrl = 'https://api.flickr.com/';
     axios({
       url: getImagesUrl,
@@ -52,23 +60,30 @@ class Gallery extends React.Component {
 
   componentDidMount() {
     window.addEventListener('resize',() => {this.setState({galleryWidth: document.body.clientWidth})});
-    this.getImages(this.props.tag);
+    this.getImages(this.props.tag,this.state.numOfImages);
     this.setState({
       galleryWidth: document.body.clientWidth
     });
   }
 
   componentWillReceiveProps(props) {
-    this.getImages(props.tag);
+    this.getImages(props.tag,this.state.numOfImages);
   }
 
   render() {
     return (
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={this.loadMore.bind(this)}
+        hasMore={true}
+        loader={<div className="loader" key={0}>Loading ...</div>}
+      >
       <div className="gallery-root">
         {this.state.images.map(dto => {
           return <Image key={'image-' + dto.id} dto={dto} galleryWidth={this.state.galleryWidth}/>;
         })}
       </div>
+      </InfiniteScroll>
     );
   }
 }
